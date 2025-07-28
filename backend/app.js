e5840d93a10ff92fde0d01eea67df6ebee49b0ea
Limbani <lchaponda@umass.edu>
2025-07-28 11:12:23 -0400

    added prismaclient, routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import { postTaskHandler, getTaskHandler, updateTaskHandler } from './controllers/taskHandlers.js';
+
 
 const app = express();
+const router = express.Router();
+
 const PORT = process.env.PORT || 3001; 
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+router.post('/task/data', postTaskHandler);
+router.get('/task/data', getTaskHandler); 
+router.update('/task/data', updateTaskHandler);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
